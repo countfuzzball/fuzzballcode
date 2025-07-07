58c0c886327e524d1610216cd1df6d28b5adbeaf
countfuzzball <49041545+countfuzzball@users.noreply.github.com>
2025-07-07 13:31:35 +0100

    Update remotevnc.sh

--- scripts/encrypt/remotevnc.sh
+++ scripts/encrypt/remotevnc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #connects to remove server and forwards the local port 5901 to port 4000 on the remote server
-ssh -v -i /mnt/Local/GCloud/AL_privatekey.pem  -nNT -R 0.0.0.0:4000:localhost:5901 -p 22 andrewmcoughlan@35.207.189.194
+ssh -v -i /mnt/Local/GCloud/AL_privatekey.pem  -nNT -R 0.0.0.0:4000:localhost:5901 -p 22 <url>
